f93387e6a6d3b97d92e511dff4077ff29a074953
mohser sh <sh1361m@gmail.com>
2021-02-20 14:58:33 +0800

    fix

--- handler.py
+++ handler.py
@@ -8,13 +8,14 @@ nlp = spacy.load("/opt/en_core_web_sm-2.2.5/en_core_web_sm/en_core_web_sm-2.2.5"
 
 def lambda_handler(event, context):    
     doc = nlp(event['body'])
-    print("--->>>1: ", doc)
+
+    results = []
     for token in doc:
-    	print("--->>>2: ",token)
-    	results = (token.text, token.pos_, token.dep_)
+    	results.append((token.text, token.pos_, token.dep_))
     return {
         'statusCode': 200,
         'body': json.dumps(results)
     }
+
 
 
--- test_api.py
+++ test_api.py
@@ -1,19 +1,8 @@
 import requests
 import json
 
-'''
-input_data = {"body": ['The sky is blue and beautiful.',
-                  'Love this blue and beautiful sky!',
-                  'The quick brown fox jumps over the lazy dog.',
-                  "A king's breakfast has sausages, ham, bacon, eggs, toast and beans",
-                  'I love green eggs, ham, sausages and bacon!',
-                  'The brown fox is quick and the blue dog is lazy!',
-                  'The sky is very blue and the sky is very beautiful today',
-                  'The dog is lazy but the brown fox is quick!'    
-            ]
-        }
-'''
-input_data = {"body": 'The sky is blue and beautiful. Love this blue and beautiful sky!The quick brown fox jumps over the lazy dog.'}
+
+input_data = {"body": "The sky is blue and beautiful. Love this blue and beautiful sky!The quick brown fox jumps over the lazy dog."}
 
 url = "https://q0cl1wwzb7.execute-api.us-east-2.amazonaws.com/dev/spacy"
 
